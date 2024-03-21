2901ab403e6257fe7e9a0f067fc035edfb5410c9
sofie <sofie@w60.fritz.box>
2024-03-21 13:16:16 +0100

    styling updates

--- ReactToDoList/src/components/ToDoList.jsx
+++ ReactToDoList/src/components/ToDoList.jsx
@@ -8,7 +8,7 @@ const ToDoList = ({task, deleteTodos}) => {
     setIsChecked(!isChecked);
   }
   return (
-    <div className="flex gap-4">
+    <div className="flex gap-4 container-item">
       <input type='checkbox' onClick={HandleChecked}/>
       <p style={{textDecoration: isChecked? 'line-through' : 'none'}}>{task.task}</p>
       <button className="border-2 p-2 border-red-500">Edit</button>
