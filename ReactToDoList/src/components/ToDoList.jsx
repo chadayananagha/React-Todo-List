e77188bdd922a7d6f6c01c74873ec6c4742f531a
sofie <sofie@w60.fritz.box>
2024-03-22 12:44:22 +0100

    installed tailwind plugin forms and styled checkboxes

--- ReactToDoList/src/components/ToDoList.jsx
+++ ReactToDoList/src/components/ToDoList.jsx
@@ -8,7 +8,11 @@ const ToDoList = ({ task, deleteTodos }) => {
   };
   return (
     <div className="flex gap-4 container-item">
-      <input type="checkbox" onClick={HandleChecked} />
+      <input
+        type="checkbox"
+        onClick={HandleChecked}
+        className="p-2 mr-2 ml-2 w-6 h-6 rounded-full shadow-inner transition-all duration-300 ease-in-out checked:shadow-xl peer bg-slate-400/20 text-sky-500 checked:bg-sky-500 hover:bg-sky-500/30 focus:ring-sky-500"
+      />
       <p
         className="w-full font-bold text-slate-700"
         style={{ textDecoration: isChecked ? "line-through" : "none" }}
